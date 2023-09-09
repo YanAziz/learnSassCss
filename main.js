b8743c7e28df7d42db998fdf1b885c49948c2524
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-09-09 15:57:48 +0700

    firstCommit

---
+++ main.js
@@ -0,0 +1,34 @@
+/*
+
+Css preposesor --> program yang memungkinkan Css dari sintaks khusus preposesor itu sendiri
+variabel, fungsi, dsb --> Css lebih terstruktur dan lebih efisien
+
+Jenis
+1. Sass
+2. Less
+3. Stylus
+4. Post Css
+5. Styled Components
+6. Css Crush
+
+*/
+
+// ----------------------------------------------------------------------------------------------------------
+
+/*
+
+Sass --> Syntactically Awesome Stylesheet 
+Free to use -- kompatibel pada semua vrsi css -- menghemat waktu dengan mengurangi pengulangan css
+https://sass-lang.com/
+
+Less --> Leaner Style Sheets
+Free to use -- mudah dan cepat dipelajari
+https://lesscss.org/
+
+Stylus --> bahasa stylesheet inovatif yang mengkompilasi ke css
+Terinspirasi oleh Sass -- dibangun dengan node.js
+https://stylus-lang.com/
+
+
+
+*/
